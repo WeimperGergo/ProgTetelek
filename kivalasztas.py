1369edeac6e52f29c6785376fd3053b283ea8231
WeimperGergo <weimper.geri@gmail.com>
2024-01-30 13:24:53 +0100

    kiválasztás tétele

---
+++ kivalasztas.py
@@ -0,0 +1,48 @@
+import math
+
+
+def kivalasztas():
+    prim = False
+    n = 9999
+    while not prim:
+        n += 1
+        i = 2
+        while i <= math.sqrt(n) and n % i != 0:
+            i += 1
+        prim = i > math.sqrt(n)
+    print(n)
+
+
+def osztok(szam):
+    szamok = []
+    for i in range(2, int(math.sqrt(szam))):
+        if szam % i == 0:
+            szamok.append(i)
+    return szamok
+
+
+def osztok_while(szam):
+    szamok = []
+    i = 2
+    while i < math.sqrt(szam):
+        if szam % i == 0:
+            szamok.append(i)
+        i += 1
+    return szamok
+
+
+def kiiras(lista):
+    if len(lista) > 0:
+        for i in range(0, len(lista)-1):
+            print(lista[i], end=" ; ")
+        print(lista[-1])
+    else:
+        print("Nincs osztója!")
+
+
+nSzam = 10007
+# szamLista = osztok(nSzam)
+# szamListaWhile = osztok_while(nSzam)
+# kiiras(szamLista)
+# kiiras(szamListaWhile)
+kivalasztas()
--- main.py
+++ main.py
@@ -1,3 +1,2 @@
 import osszegzes
 
-osszegzes.osszegez()
